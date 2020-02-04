e7ce257a58953c38acb63096c3923fe9d7d7ef6b
Vitalik Melnyk <melnyk.vitalikk@gmail.com>
2020-02-04 18:38:15 +0200

    create axios get request and display cards

---
+++ src/containers/DictionaryBox.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from "react";
+
+// Connect axios
+import axios from "axios";
+
+// Connect react-bootstrap
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+
+import "./DictionaryBox.css";
+
+const URBAN_KEY = {
+  headers: {
+    "x-rapidapi-key": "fd7f461f40mshb035cc0f7e57873p193233jsna5e723db7381"
+  }
+};
+
+export const DictionaryBox = props => {
+  const [wordCard, setWordCard] = useState([]);
+  const [word, setWord] = useState("");
+
+  const handleChange = event => {
+    // event.preventDefault();
+    console.log(event.target.value);
+    setWord(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    console.log("hello");
+    axios
+      .get(
+        `https://mashape-community-urban-dictionary.p.rapidapi.com/define?term=${word}`,
+        URBAN_KEY
+      )
+      .then(res => {
+        console.log(res.data.list);
+        const data = res.data.list;
+        setWordCard(data);
+        // console.log(wordCard);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+  //   useEffect(() => {
+  //     handleSubmit();
+  //   }, []);
+
+  return (
+    <>
+      <section>
+        <Container className="">
+          <Row className="">
+            <Col className="text-center">
+              <h1>Urban Dictionary</h1>
+            </Col>
+          </Row>
+          <Form className="form" onClick={handleSubmit}>
+            <Row className="text-center">
+              <Col className="">
+                <input
+                  type="text"
+                  className="form-control form-input"
+                  id="urban-words"
+                  placeholder="Enter any word what you want..."
+                  onChange={handleChange}
+                />
+                <Button variant="outline-primary" className="">
+                  Find words
+                </Button>
+              </Col>
+            </Row>
+          </Form>
+          <p>{word}</p>
+          <Row className="cards-box">
+            {wordCard.map(card => (
+              <Col key={card.defid}>
+                <div className="card-item">
+                  <p>{card.definition}</p>
+                  <p>{card.word}</p>
+                </div>
+              </Col>
+            ))}
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+};
